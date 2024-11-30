9f4fd232f1d4a4607e7f6cd23aaaa33c87283468
LuisaWeisch <luisa.weisch@outlook.com>
2024-11-30 23:36:16 +0100

    defining new function and augmenting mean of specific gene sets

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -2,9 +2,17 @@
 # Function to extract the metadata for a given fratures from the loaded data
 extract_feature <- function(feature, metadata) {
   metadata %>%
-    str_subset(paste0("^", feature)) %>%
+    str_subset(str_c("^", feature)) %>%
     str_split("\t", simplify = TRUE) %>%
     as.vector() %>%
     .[-1] %>%
     str_remove_all("\"")
+}
+
+
+find_genes <- function(gene_set, all_genes){
+  present_genes <- sapply(gene_set, function(gene){
+    grep(gene, all_genes, ignore.case = TRUE, value = TRUE)}) %>%
+    unlist()
+  return(present_genes)
 }
