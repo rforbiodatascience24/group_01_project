befa25a84ceec4c8364b35fe227ab99d24d9242d
RuneDaucke32 <runda@dtu.dk>
2024-12-01 13:25:29 +0100

    added R script

---
+++ R/99_proj_func.R
@@ -0,0 +1,12 @@
+# Functions for 02_clean_qmd
+## Function to map probe IDs to gene names
+map_probe_to_gene <- function(gene_probe_ids) {
+  gene_names <- gene_probe_ids |>
+    sapply(function(probe_id) {
+      mapped_genes <- ifelse(is.na(probe_id) || probe_id == "", "Unnamed",
+                             ifelse(is.null(getSYMBOL(probe_id, "hgu133a2.db")), probe_id, getSYMBOL(probe_id, "hgu133a2.db")))
+      return(mapped_genes)
+    })
+  ## Ensure no duplicates or missing values in gene names
+  return(make.unique(ifelse(is.na(gene_names), "Unnamed", gene_names)))
+}
